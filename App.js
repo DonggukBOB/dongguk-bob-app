b0b2fe483b84e0e41413f01263f623f804d985d8
hwookim <gusdn9754@gmail.com>
2021-05-27 15:48:38 +0900

    style: 상태표시줄 색상 변경

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Constants } from "react-native-unimodules";
 import WebView from "react-native-webview";
 
 export default function App() {
   return (
-    <WebView
-      source={{ uri: "https://donggukbob.github.io/dongguk-bob/" }}
-      style={styles.container}
-    />
+    <>
+      <View style={styles.statusBar} />
+      <WebView
+        source={{ uri: "https://donggukbob.github.io/dongguk-bob/" }}
+        style={styles.container}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  statusBar: {
+    height: Constants.statusBarHeight,
+    backgroundColor: "#f29300",
+  },
   container: {
-    marginTop: Constants.statusBarHeight,
     alignItems: "center",
     justifyContent: "center",
   },
